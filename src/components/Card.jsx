55fcec4d1f14ba53c106caec9f535503faaa3a9a
JesusVA760 <tonio4340406@gmail.com>
2024-01-18 21:56:09 -0800

    progress

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import Hero from "../assets/images/illustration-d.svg";
+import Mobilehero from "../assets/images/illustration-m.svg";
 import "./card.css";
 
-export default function card() {
+export default function Card({ email, setEmail, setSubmitted }) {
   return (
     <div className="card">
+      <img src={Mobilehero} className="mobile-hero" />
+
       <div className="card-content">
         <h1>Stay Updated!</h1>
         <p className="card-subtitle">
@@ -16,10 +19,23 @@ export default function card() {
           <li>And much more!</li>
         </ul>
         <p className="card-email">Email address</p>
-        <input type="email" placeholder="email@company.com" />
-        <button>Subscribe to monthly newsletter</button>
+        <input
+          type="email"
+          required
+          placeholder="email@company.com"
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <button
+          onClick={() => {
+            if (email) {
+              setSubmitted(true);
+            }
+          }}
+        >
+          Subscribe to monthly newsletter
+        </button>
       </div>
-      <img src={Hero} />
+      <img src={Hero} className="hero" />
     </div>
   );
 }
