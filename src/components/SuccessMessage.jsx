10761087bd9999d829c8e06e0d6cbe4e643b6ebd
JesusVA760 <tonio4340406@gmail.com>
2024-01-19 13:07:15 -0800

    progress

--- src/components/SuccessMessage.jsx
+++ src/components/SuccessMessage.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import SuccessCheck from "../assets/images/icon-list.svg";
+import "./SuccessMessage.css";
 
 export const SuccessMessage = ({ email }) => {
   return (
-    <div>
-      <p>{email}</p>
+    <div className="successContainer">
+      <div className="successContent">
+        <img src={SuccessCheck} className="successCheckmark" />
+        <h1>Thank you for subscribing!</h1>
+        <p>
+          A confirmation email has been sent to{" "}
+          <span className="emailSent">{email} </span>. Please open it and click
+          the button inside to confirm your subscription
+        </p>
+      </div>
+      <button>Dismiss message</button>
     </div>
   );
 };
